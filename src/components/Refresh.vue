75463c4a0ac2b462618d6598bccc156b4d07b4fe
Grzegorz Bylica <grzegorz.bylica@gmail.com>
2020-05-26 13:28:24 +0200

    #3 Added new component SimpleTable.vue
    Correction of the Refresh component.
    
    resolve #3

--- src/components/Refresh.vue
+++ src/components/Refresh.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="refresh">
+    <div v-bind:class='{
+                        "gb-min-alert-refresh": isAlert(),
+                        "gb-min-warning-refresh": isWarning(),
+                        "gb-min-ok-refresh": isOk()
+                    }'>
         <slot></slot>
-        Test
+        Last Refresh: <span>{{lastDateRun | moment("yyyy-MM-dd h:mm:ss")}}</span>
     </div>
 </template>
 
@@ -9,14 +13,39 @@
     export default {
         data() {
             return {
-
+                history: [],
+                lastDateRun: new Date(),
+                interval: 1000  // miliseconds
             }
+        },
+        methods: {
+            isAlert() {
+                return (new Date() - this.lastDateRun > 10000)
+            },
+            isWarning() {
+                return (new Date() - this.lastDateRun > 50000)
+            },
+            isOk() {
+                return (new Date() - this.lastDateRun >= 2000)
+            }
+
         }
     }
 </script>
 
 <style scoped>
-    .refresh {
+    .gb-min-alert-refresh {
         background-color: red;
+        color: white;
+    }
+
+    .gb-min-warning-refresh {
+        background-color: orange;
+        color: #2c3e50;
+    }
+
+    .gb-min-ok-refresh {
+        background-color: white;
+        color: grey;
     }
 </style>
